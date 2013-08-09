a1a6f4c66ae4cdf63594eb0195da34e24fc51267
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-09 14:04:55 +0200

    Pretty printer.

--- program.rb
+++ program.rb
@@ -1,11 +1,27 @@
 require 'rspec-expectations'
+require 'sxp'
 
 module BV
+  def self.parse input
+    Program.new SXP.parse(input)
+  end
+  
   class Program
-    def self.parse sexp
+    def initialize sexp
       sexp[0].should == :lambda
       x = sexp[1][0]
       @program = Expression.parse sexp[2], x: x
+    end
+    
+    def to_sexp
+      [:lambda, [:x], @program.to_sexp]
+    end
+    
+    def to_s
+      sio = StringIO.new
+      gen = SXP::Generator.new sio
+      gen.render to_sexp
+      sio.string.strip
     end
   end
   
@@ -30,7 +46,10 @@ module BV
 
   def self.Constant n
     cls = Class.new(Expression) do
-      const_set :VALUE, n
+      define_method(:value){ n }
+      def to_sexp
+        value
+      end
     end
   end
   
@@ -39,7 +58,10 @@ module BV
 
   def self.Variable i
     cls = Class.new(Expression) do
-      const_set :INDEX, i
+      define_method(:index){ i }
+      def to_sexp
+        index
+      end
     end
   end
   
@@ -52,6 +74,10 @@ module BV
       @condition = Expression.parse sexp[0], context
       @when_zero = Expression.parse sexp[1], context
       @else = Expression.parse sexp[2], context
+    end
+    
+    def to_sexp
+      [:if0, @condition.to_sexp, @when_zero.to_sexp, @else.to_sexp]
     end
   end
   
@@ -66,42 +92,55 @@ module BV
       context[:y], context[:z] = p
       @expr = Expression.parse e, context
     end
+
+    def to_sexp
+      [:fold, @arg.to_sexp, @acc.to_sexp, [:lambda, [:y, :z], @expr.to_sexp]]
+    end
+  end
+  
+  class Op1 < Expression
+    def initialize sexp, context
+      @arg = Expression.parse sexp[0], context
+    end
+    
+    def to_sexp
+      [op, @arg.to_sexp]
+    end
   end
   
   def self.Op1 op
-    cls = Class.new(Expression) do
-      const_set :OP, op
-      
-      def initialize sexp, context
-        @arg = Expression.parse sexp[0], context
-      end
+    cls = Class.new(Op1) do
+      define_method(:op){ op }
     end
   end
   
   %w(not shl1 shr1 shr4 shr16).each do |op|
-    const_set op.capitalize.to_s, Op1(op)
+    const_set op.capitalize.to_s, Op1(op.to_sym)
   end
 
   def self.Op2 op
     cls = Class.new(Expression) do
-      const_set :OP, op
+      define_method(:op){ op }
       
       def initialize sexp, context
         @left = Expression.parse sexp[0], context
         @right = Expression.parse sexp[1], context
       end
+      
+      def to_sexp
+        [op, @left.to_sexp, @right.to_sexp]
+      end
     end
   end
 
-  And = Op2(:&)
-  Or = Op2(:|)
-  Xor = Op2(:^)
-  Plus = Op2(:+)
+  %w(and or xor plus).each do |op|
+    const_set op.capitalize.to_s, Op2(op.to_sym)
+  end
 end
 
 __END__
 
 load 'program.rb'
 require 'sxp'
-sexp = SXP.parse "(lambda (x_8067) (if0 (shl1 (and (shl1 1) x_8067)) 0 x_8067))"
-p BV::Program::parse sxp
+prg = BV.parse "(lambda (x_3597) (fold x_3597 0 (lambda (x_3597 x_3598) (and (shr4 x_3597) x_3597))))"
+puts prg.to_s
