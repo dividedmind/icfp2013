9e63fad2afecb14688d79baefdb6beb371456bdd
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-09 19:20:26 +0200

    Prune solutions from feedback.

--- submitter.rb
+++ submitter.rb
@@ -6,8 +6,8 @@ require 'bv'
 
 size = 6
 operators = []
-#problems = JSON.parse RestClient.get('http://icfp2013lf.herokuapp.com/myproblems?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', nil)
-problems = [JSON.parse(RestClient.post('http://icfp2013lf.herokuapp.com/train?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', {size: size, operators: operators}.to_json))]
+problems = JSON.parse RestClient.get('http://icfp2013lf.herokuapp.com/myproblems?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', nil)
+# problems = [JSON.parse(RestClient.post('http://icfp2013lf.herokuapp.com/train?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', {size: size, operators: operators}.to_json))]
 problems = problems.find_all {|a| a['size'] == size && (!a['solved']) && (a['timeLeft'] || 42) > 0}
 #problems = [{"id"=>"3OVf2IsEAbkuaqElXg7Ax3je", "size"=>5, "operators"=>["or", "shl1"], "solved"=>false, "expires_at"=>nil, "solution"=>nil, "kind"=>"contest"}]
 problems.each do |problem|
@@ -15,18 +15,27 @@ problems.each do |problem|
   next unless operators.all?{|x| ops.include? ops}
   p problem
   solutions = BV.generate size: problem['size'], operators: ops
-  puts "%d solutions" % solutions.length
-  next if solutions.length > 25
+  puts "%d solutions total." % solutions.length
+  next if solutions.length > 300
   reqs = 0
   start_time = Time.now
-  solutions.shuffle.each do |sol|
+  while solutions.length > 0
+    puts "%d solutions left." % solutions.length
+    sol = solutions.shuffle.first
     program = sol.to_s
     puts program
     reqs += 1
     result = JSON.parse(RestClient.post('http://icfp2013lf.herokuapp.com/guess?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', {id: problem["id"], program: program}.to_json)) rescue {}
     p result
-    break if result['status'] == 'win'
-    raise "couldn't solve!!" if reqs == solutions.length
+    case result['status']
+    when 'win'
+      break
+    when 'mismatch'
+      solutions.reject! {|s| s.eval(Integer(result['values'][0])) != Integer(result['values'][1])}
+    else
+      raise "unknown response: #{result}"
+    end
+    raise "couldn't solve!!" if solutions.length < 1
   end
   puts "Solved in %d requests (%s s)." % [reqs, Time.now - start_time]
 end
