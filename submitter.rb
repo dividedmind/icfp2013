52d713215826ef7fcb8cbc6230b6e2c7fc16c62e
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-09 18:58:22 +0200

    Show solution time.

--- submitter.rb
+++ submitter.rb
@@ -4,11 +4,11 @@ require 'rest-client'
 $: << '.'
 require 'bv'
 
-size = 5
+size = 6
 operators = []
 #problems = JSON.parse RestClient.get('http://icfp2013lf.herokuapp.com/myproblems?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', nil)
 problems = [JSON.parse(RestClient.post('http://icfp2013lf.herokuapp.com/train?auth=0229KtQKyHAgd8LaD0JPubHAC9InNBjCPTxnhVQBvpsH1H', {size: size, operators: operators}.to_json))]
-problems = problems.find_all {|a| a['size'] == size && (!a['solved'])}
+problems = problems.find_all {|a| a['size'] == size && (!a['solved']) && (a['timeLeft'] || 42) > 0}
 #problems = [{"id"=>"3OVf2IsEAbkuaqElXg7Ax3je", "size"=>5, "operators"=>["or", "shl1"], "solved"=>false, "expires_at"=>nil, "solution"=>nil, "kind"=>"contest"}]
 problems.each do |problem|
   ops = problem['operators']
@@ -18,6 +18,7 @@ problems.each do |problem|
   puts "%d solutions" % solutions.length
   next if solutions.length > 25
   reqs = 0
+  start_time = Time.now
   solutions.shuffle.each do |sol|
     program = sol.to_s
     puts program
@@ -27,5 +28,5 @@ problems.each do |problem|
     break if result['status'] == 'win'
     raise "couldn't solve!!" if reqs == solutions.length
   end
-  puts "Solved in %d requests." % reqs
+  puts "Solved in %d requests (%s s)." % [reqs, Time.now - start_time]
 end
