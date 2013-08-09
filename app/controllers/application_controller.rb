ecf43077ecd67b43d10fba34673275c6f4907bb7
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-09 07:57:56 +0200

    Check authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :ensure_auth
+  
+  def ensure_auth
+    render text: "Please provide valid ?auth", status: :unauthorized unless params[:auth] == ENV['ICFP_AUTH_KEY']
+  end
 end
