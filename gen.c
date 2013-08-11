4b1e98c489364b5fc40e727723eb9ec2a51340d7
Rafał Rzepecki <divided.mind@gmail.com>
2013-08-11 11:20:45 +0200

    Only take allowed ops.

--- gen.c
+++ gen.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #include "gen.h"
 #include "eval.h"
@@ -9,11 +8,6 @@
 // returns nonzero on failure
 static char check_ops(bv_expr expr, bv_mask allowed, bv_mask needed)
 {
-  if (allowed >= BV_FOLD_MASK) {
-    puts("folds not handled yet");
-    return -1;
-  }
-  
   for (int i = 0; i < expr.size; i++) {
     bv_mask mask = 1 << (expr.code & 0xf);
     if (!(mask & allowed))
@@ -34,11 +28,16 @@ bv_expr gen_solution(bv_problem problem, bv_example *examples, size_t excount)
 
   bv_expr sol;
   
-  srandom(time(NULL));
-  
   for(;;) {
-    sol.size = problem.size - 1;
-    sol.code = random();
+    sol.size = 0;
+    sol.code = 0;
+    
+    while (sol.size < problem.size - 1) {
+      char op = rand() & 0xf;
+      if (!((1 << op) & allowed_ops)) continue;
+      sol.code = (sol.code << 4) | op;
+      sol.size++;
+    }
     
     if ((sol.size = bv_eval_program(sol, 0, NULL)) == 0) continue;
     if (check_ops(sol, allowed_ops, needed_ops)) continue;
--- solve.c
+++ solve.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "webapi.h"
 #include "gen.h"
@@ -6,14 +8,11 @@
 
 int main(int argc, char **argv)
 {
-  bv_problem prob = get_training_problem(5);
+  srand(time(NULL));
+
+  bv_problem prob = get_training_problem(10);
   printf("%x %lx\n", prob.size, prob.ops);
   
-  if (prob.ops >= BV_FOLD_MASK) {
-    puts("can't handle folds yet");
-    return 0;
-  }
-
   bv_example examples[1024];
   int excount = 0;
 
